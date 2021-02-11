804c89cf4fa34baa590b46450bf4bfcc158a65f4
Melquias Ribeiro <melquias10.mr@gmail.com>
2021-02-11 17:44:47 -0300

    config: initial style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
     return (
         <>
             <BrowserRouter>
+                <GlobalStyle />
+                <ToastContainer
+                    position="top-center"
+                    autoClose={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                />
                 <Routes />
             </BrowserRouter>
         </>
